a6f0b29ee7ecae1274f1f5ea1cd855aab5f59b28
Diego von Ziegler <diegovonziegler@gmail.com>
2023-02-14 16:45:12 -0300

    Tercera escritura en repositorio

--- CV-DVZ/script/script.js
+++ CV-DVZ/script/script.js
@@ -19,7 +19,9 @@ document.getElementById("BotonEspañol").onclick = function () {
 document.getElementById("BotonEnglish").onclick = function () {
     document.getElementById("ParrafoProfesion").innerHTML ="Engineer"
 }
-
+ListaDesordenadaLargaIP
+BotonVerMenosInformacionPersonal
+BotonSeeMorePersonalInformation
 */
 
 document.getElementById("BotonEspañol").addEventListener('click', function () {
@@ -35,7 +37,10 @@ document.getElementById("BotonEspañol").addEventListener('click', function () {
     document.getElementById("SeccionL3E").style.display = 'block',
     document.getElementById("SeccionR3E").style.display = 'block',
     document.getElementById("SeccionL4E").style.display = 'block',
-    document.getElementById("SeccionR4E").style.display = 'block'
+    document.getElementById("SeccionR4E").style.display = 'block',
+    document.getElementById("ListaDesordenadaLargaIP").style.display = 'none',
+    document.getElementById("ListaDesordenadaLargaExperiencias").style.display = 'none',
+    document.getElementById("ListaDesordenadaLargaHabilidades").style.display = 'none'
 }
    )
 
@@ -52,7 +57,61 @@ document.getElementById("BotonEnglish").addEventListener('click', function () {
     document.getElementById("SeccionL3I").style.display = 'block',
     document.getElementById("SeccionR3I").style.display = 'block',
     document.getElementById("SeccionL4I").style.display = 'block',
-    document.getElementById("SeccionR4I").style.display = 'block'
+    document.getElementById("SeccionR4I").style.display = 'block',
+    document.getElementById("ListaDesordenadaLargaPI").style.display = 'none',
+    document.getElementById("ListaDesordenadaLargaExperiences").style.display = 'none',
+    document.getElementById("ListaDesordenadaLargaSkills").style.display = 'none'
 }
    )
+document.getElementById("BotonVerMasInformacionPersonal").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaIP").style.display = 'block'
+}
+    )
+document.getElementById("BotonVerMenosInformacionPersonal").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaIP").style.display = 'none'  
+}
+    )
+document.getElementById("BotonSeeMorePersonalInformation").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaPI").style.display = 'block'   
+}
+    ) 
+document.getElementById("BotonSeeLessPersonalInformation").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaPI").style.display = 'none'      
+}
+    )
 
+
+document.getElementById("BotonVerMasExperiencias").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaExperiencias").style.display = 'block'
+}
+    )
+document.getElementById("BotonVerMenosExperiencias").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaExperiencias").style.display = 'none'  
+}
+    )
+ document.getElementById("BotonSeeMoreExperiences").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaExperiences").style.display = 'block'   
+}
+    ) 
+document.getElementById("BotonSeeLessExperiences").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaExperiences").style.display = 'none'      
+}
+    )
+
+
+document.getElementById("BotonVerMasHabilidades").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaHabilidades").style.display = 'block'
+}
+    )
+document.getElementById("BotonVerMenosHabilidades").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaHabilidades").style.display = 'none'  
+}
+    )
+document.getElementById("BotonSeeMoreSkills").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaSkills").style.display = 'block'   
+}
+    ) 
+document.getElementById("BotonSeeLessSkills").addEventListener('click', function () {
+    document.getElementById("ListaDesordenadaLargaSkills").style.display = 'none'      
+}
+    )
