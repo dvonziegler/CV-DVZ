d9c9fdb9dd2e1d8bb8a8daf77e6cbde4191fc972
Diego von Ziegler <diegovonziegler@gmail.com>
2023-02-20 11:08:05 -0300

    Quinta escritura en repositorio

--- CV-DVZ/script/script.js
+++ CV-DVZ/script/script.js
@@ -29,11 +29,16 @@ for(let i = 0; i < collection.length; i++) {
 }
 */
 
+/*Al iniciar la pagina se ocultan las fila 2, 3 y 4 hasta que el usuario elija un idioma
+*/
 const collection = document.getElementsByClassName("OcultarAlInicio");
 for(let i = 0; i < collection.length; i++) {
     collection[i].style.display = 'none'
 }
 
+/*Al presionar el boton de algun idioma se muesta la informacion de las filas 2, 3 y 4
+ e inmediatamente se oculta la informacion del idioma que no ha sido seleccionado
+*/
 document.getElementById("BotonEspañol").addEventListener('click', function() {
     const collection = document.getElementsByClassName("OcultarAlInicio");
     for(let i = 0; i < collection.length; i++) {
@@ -88,6 +93,11 @@ document.getElementById("BotonEnglish").addEventListener('click', function () {
     document.getElementById("ListaDesordenadaLargaSkills").style.display = 'none'
 }
    )
+
+/*Al presionar el boton ver mas/see more agrega 3 items de informacion adicional.
+Al presionar el boton ver menos/see less oculta los 3 items de informacion adicional agregados
+anteriormente
+*/
 document.getElementById("BotonVerMasInformacionPersonal").addEventListener('click', function () {
     document.getElementById("ListaDesordenadaLargaIP").style.display = 'block'
 }
